e74000e1ec9682beeb3b63fe182cdabed3c4dd91
DMXMax <29312988+DMXMax@users.noreply.github.com>
2018-09-10 15:37:06 +0000

    Improved rendering speed, and added better menu logo

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../../hoc/AuxFile';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props)=>{
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+  componentWillUpdate(){
+    console.log('[Component Updating]');
+  }
+  render() {
+
+  const ingredientSummary = Object.keys(this.props.ingredients)
     .map(key=>{
       return (
         <li key={key}>
-        <span style={{textTransform:'capitalize'}}>{key}</span >: {props.ingredients[key]}
+        <span style={{textTransform:'capitalize'}}>{key}</span >: {this.props.ingredients[key]}
         </li>
       );
     });
-  return (
+
+    return (
     <Aux>
       <h3>Your Order</h3>
       <p>Made fresh for you with the following ingredients:</p>
       <ul>
       {ingredientSummary}
       </ul>
-      <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
+      <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
       <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+      <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+      <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
     </Aux>
   );
 };
-
-export default orderSummary
+}
+export default OrderSummary
