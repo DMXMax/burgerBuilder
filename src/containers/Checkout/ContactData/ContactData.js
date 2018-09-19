4dc4ddf44a2c25865d90ced0df348c4bf3959fe3
DMXMax <29312988+DMXMax@users.noreply.github.com>
2018-09-19 15:19:04 +0000

    Finally managed to get dynamic styling on the form. This chapter was a bit of a hot mess, but it might have been me jumping around.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,14 +7,60 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component{
   state={
-    name : '',
-    email : '',
-    address: {
-      street: '',
-      postalCode: ''
+    orderForm:{
+      name:{
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name',
+        },
+        value: '',
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street',
+        },
+        value: '',
+      },
+      zipcode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Zip Code',
+        },
+        value: '',
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country',
+        },
+        value: '',
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Email Address',
+        },
+        value: '',
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options:[
+            {value: 'fastest', displayValue: 'Fast'},
+            {value: 'cheapest', displayValue: 'Cheap'}
+          ]
+        },
+        value: '',
+      },
     },
     loading:false,
-  };
+  }
 
   orderHandler = (event) =>{
     event.preventDefault();
@@ -45,16 +91,30 @@ class ContactData extends Component{
 
   render(){
     let form  = (null);
-
+    const formElementsArray = [];
+    for( let key in this.state.orderForm){
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key],
+      })
+    };
     if(this.state.loading === true)
       form = <Spinner/>
     else
       form = (
         <form>
-        <Input inputtype="input" type='text' name='name' placeholder='Your Name' />
+        {formElementsArray.map(formElement=>(
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+          />
+        ))}
+        {/*<Input elementType="..." elementConfig ="..." value="...">
         <Input inputtype="input" type='email' name='email' placeholder='Your E-mail' />
         <Input inputtype="input" type='text' name='street' placeholder='Street' />
-        <Input inputtype="input" type='text' name='postalCode' placeholder='Zip Code' />
+        <Input inputtype="input" type='text' name='postalCode' placeholder='Zip Code' />*/}
         <Button clicked={this.orderHandler} btnType="Success">MAKE IT SO</Button>
         {/*<Button btnType="Danger">NO, I REFUSE</Button>*/}
       </form>
