b934432ca909d09c90aa816a74b0305111bef6c4
DMXMax <29312988+DMXMax@users.noreply.github.com>
2018-09-25 14:57:49 +0000

    added redux-thunk

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import burgerBuilder from './store/reducers/burgerBuilder';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(burgerBuilder,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+ composeEnhancers(
+  applyMiddleWare(thunk)
+ )
+
 );
+
 
 const app = (
   <Provider store={store}>
